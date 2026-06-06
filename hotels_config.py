e6865e6c78688fc10f8478c8640462cfa3e04d3c
lucas8053-cyber <lucas8053@gmail.com>
2026-06-06 15:53:39 +0800

    Update hotels_config.py

--- hotels_config.py
+++ hotels_config.py
@@ -1,7 +1,7 @@
-# hotels_config.py (備用方案：關鍵字精確匹配)
+# hotels_config.py
 MONITORED_HOTELS = [
-    "合樂商務設計旅店 台中",
-    "夏都汽車旅館 台中",
-    "金沙時尚汽車旅館 台中",
-    "風緻汽車旅館 台中"
+    {"name": "合樂商務設計旅店", "data_id": "ChIJIZInA_Q9aTQRc3-7Ov1eSyc"},
+    {"name": "夏都汽車旅館", "data_id": "ChIJERI-H9gXaTQR36NvL-P5E2s"},
+    {"name": "金沙時尚汽車旅館", "data_id": "ChIJ74BoswU9aTQRkcBfnvWLH2k"},
+    {"name": "風緻汽車旅館", "data_id": "ChIJ4_9nFfs8aTQR4Rg9SPCC8dY"}
 ]
