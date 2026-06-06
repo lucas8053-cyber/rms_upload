2ca7613444e5ee7085ff4aac832211e5d5f0a172
lucas8053-cyber <lucas8053@gmail.com>
2026-06-06 15:24:17 +0800

    Update hotels_config.py

--- hotels_config.py
+++ hotels_config.py
@@ -1,7 +1,7 @@
 # hotels_config.py
 MONITORED_HOTELS = [
-    "合樂商務設計旅店",
-    "夏都汽車旅館",
-    "金沙時尚汽車旅館",
-    "風緻汽車旅館"
+    "合樂商務設計旅店 Taichung",
+    "夏都汽車旅館 Taichung",
+    "金沙時尚汽車旅館 Taichung",
+    "風緻汽車旅館 Taichung"
 ]
